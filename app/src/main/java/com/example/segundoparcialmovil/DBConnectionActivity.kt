3c9fea0a3e6ed13e2b6cc1643004c5b68549e816
Diego Quiñones <diegoqr2000@gmail.com>
2023-03-30 21:24:44 -0400

    Añadida la opicon de guardar en la tabla book un libro mediante los campos title y description en la actividad DBConnectionActivity

--- app/src/main/java/com/example/segundoparcialmovil/DBConnectionActivity.kt
+++ app/src/main/java/com/example/segundoparcialmovil/DBConnectionActivity.kt
@@ -4,21 +4,43 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings.Global
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class DBConnectionActivity : AppCompatActivity() {
+    private val bookTitle: EditText
+        get() = findViewById(R.id.pt_title)
+    private val bookDescription: EditText
+        get() = findViewById(R.id.pt_description)
+    private val btnSaveDataBase: Button
+        get() = findViewById(R.id.btn_savedatabase)
+    private val btnShowDataBase: Button
+        get() = findViewById(R.id.btn_showdatabase)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dbconnection)
 
-        GlobalScope.launch {
-            val bookDao = AppRoomDatabase.getDatabase(applicationContext).bookDato()
-            val repository = BookRepository(bookDao)
-            repository.insert(Book("Cien años de soledad", "Libro de Gabriel Garcia Marquez"))
-            val list = repository.getListBooks()
-            list.forEach{
-                Log.d("DBTEST", "Id book = ${it.id}, Title: ${it.title}, Desc: ${it.description}")
+
+        btnSaveDataBase.setOnClickListener{
+            GlobalScope.launch {
+                val bookDao = AppRoomDatabase.getDatabase(applicationContext).bookDato()
+                val repository = BookRepository(bookDao)
+
+                repository.insert(Book(bookTitle.text.toString(), bookDescription.text.toString()))
+            }
+        }
+
+        btnShowDataBase.setOnClickListener{
+            GlobalScope.launch {
+                val bookDao = AppRoomDatabase.getDatabase(applicationContext).bookDato()
+                val repository = BookRepository(bookDao)
+
+                val list = repository.getListBooks()
+                list.forEach{
+                    Log.d("DBTEST", "Id book = ${it.id}, Title: ${it.title}, Desc: ${it.description}")
+                }
             }
         }
     }
