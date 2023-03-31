661ba0fe343facce0e43f3b8068c82a4f00040f2
Diego Quiñones <diegoqr2000@gmail.com>
2023-03-30 20:11:21 -0400

    Se implemento el RecyclerView para mostra la lsita de POST con los respectivos campos

--- app/src/main/java/com/example/segundoparcialmovil/MainActivity.kt
+++ app/src/main/java/com/example/segundoparcialmovil/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.segundoparcialmovil
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,18 +20,32 @@ class MainActivity : AppCompatActivity() {
         val endPoint = restApiAdapter.connectionApi()
         val bookResponseCall = endPoint.getAllPost()
 
+        var mainActivity = this
+
+
         bookResponseCall.enqueue(object: Callback<List<Post>>{
             override fun onFailure(call: Call<List<Post>>, t: Throwable) {
                 t?.printStackTrace();
             }
 
             override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
-                val posts = response?.body()
+                val posts = response.body()
                 Log.d("Resp Post", Gson().toJson(posts))
-                posts?.forEach{
-                    Log.d("Resp Post", it.body)
+                val postsView = ArrayList<Post>()
+                posts?.forEach {
+                    postsView.add(Post(it.userId, it.id, it.title, it.body))
                 }
+
+                val linearLayoutManager = LinearLayoutManager(mainActivity)
+
+                linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
+                recyclerView = findViewById(R.id.rv_postView)
+                recyclerView.layoutManager = linearLayoutManager
+
+                recyclerView.adapter = PostListAdapter(postsView, mainActivity)
             }
         })
+
+
     }
 }
